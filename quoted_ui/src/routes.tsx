072985b1d806f5e459fec631df697b5a2fe3e1f3
devklick <devklick@gmail.com>
2024-10-19 11:53:44 +0100

    Try fix routing

--- quoted_ui/src/routes.tsx
+++ quoted_ui/src/routes.tsx
@@ -5,19 +5,27 @@ import Home from "./Home";
 import PageContent from "./components/Page";
 import { NavItem } from "./Layout/Navigation/NavList";
 
+const basePath = import.meta.env.BASE_URL || "/";
+
 interface RouteDefinition {
   path: string;
 }
 
 function defineRoute(path: string): RouteDefinition {
-  return { path };
+  return {
+    path: [basePath, path]
+      .filter(Boolean)
+      .map((p) => (p.endsWith("/") ? p : p + "/"))
+      .join("")
+      .slice(0, -1),
+  };
 }
 
 export const routeDefinitions = {
-  shows: defineRoute("/shows"),
-  showQuotes: defineRoute("/show/quotes"),
-  characters: defineRoute("/characters"),
-  randomQuote: defineRoute("/quote/random"),
+  shows: defineRoute("shows"),
+  showQuotes: defineRoute("show/quotes"),
+  characters: defineRoute("characters"),
+  randomQuote: defineRoute("quote/random"),
 } as const;
 
 const navItems: Array<NavItem> = [
@@ -25,8 +33,6 @@ const navItems: Array<NavItem> = [
   { title: "Characters", path: routeDefinitions.characters.path },
   { title: "Random Quote", path: routeDefinitions.randomQuote.path },
 ];
-
-const basePath = import.meta.env.DEV ? "/" : "/quoted";
 
 const routes: Array<RouteObject> = [
   {
--- quoted_ui/vite.config.ts
+++ quoted_ui/vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
     plugins: [react(), svgr()],
-    base: "quoted",
+    base: command === "build" ? "quoted" : undefined,
     server: {
       cors: true,
       proxy: {

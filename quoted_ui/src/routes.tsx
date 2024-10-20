f58c15d7e08c7595a158fe63c124d03b5120c42c
devklick <devklick@gmail.com>
2024-10-20 13:29:43 +0100

    Add todo

--- quoted_ui/src/routes.tsx
+++ quoted_ui/src/routes.tsx
@@ -7,6 +7,31 @@ import { NavItem } from "./Layout/Navigation/NavList";
 
 const basePath = import.meta.env.BASE_URL || "/";
 
+/*
+  TODO: Reset or retain scroll position based on action. 
+  Currently, when navigating from one page to another (e.g. /shows to / (home)), 
+  the scroll position from scrolls is persisted when you reach home, 
+  so that if you were half way down the /shows page, you end up half way down
+  the home page. This is unwanted. 
+
+  Ideally, if navigating via a UI button/navlink, the content scroll should 
+  always be reset. 
+
+  However, if navigating forwards or backwards via browser controls, you should 
+  automatically be scrolled to wherever you were on that page previously. 
+  
+  For example, 
+    - Scroll 100px down home page
+    - Navigate to shows page - auto scroll to top of scrolls page
+    - Scroll 200px down shows page
+    - Click browser back button - auto scroll to 100px on home page
+    - Click browser forward button - auto scroll to 200px on shows page
+
+  The <ScrollRestoration/> component seems like it should take care of this, 
+  but I cant get it to work. 
+  https://reactrouter.com/en/main/components/scroll-restoration
+*/
+
 interface RouteDefinition {
   path: string;
 }

0abc23e44646a5fcde1dda2bfcb51839de880cd1
devklick <devklick@gmail.com>
2024-10-21 21:56:13 +0100

    ui. back to browser router

--- quoted_ui/src/main.tsx
+++ quoted_ui/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import routes from "./routes.tsx";
@@ -9,7 +9,7 @@ import "./index.css";
 
 const queryClient = new QueryClient();
 
-const router = createHashRouter(routes);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

a11fb20b8791f09c5486276b30fa62408027aff2
devklick <devklick@gmail.com>
2024-10-21 21:49:54 +0100

    ui, switch to hash router

--- quoted_ui/src/main.tsx
+++ quoted_ui/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import routes from "./routes.tsx";
@@ -9,7 +9,7 @@ import "./index.css";
 
 const queryClient = new QueryClient();
 
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

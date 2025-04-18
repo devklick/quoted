79a5cb278ce3b84a0ff15c630859e3df0e863a15
devklick <devklick@gmail.com>
2025-04-18 12:04:48 +0100

    Optimize tabler icons

--- quoted_ui/vite.config.ts
+++ quoted_ui/vite.config.ts
@@ -6,7 +6,13 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
     plugins: [react(), svgr()],
-
+    resolve: {
+      alias: {
+        // See https://github.com/tabler/tabler-icons/issues/1233#issuecomment-2428245119
+        // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+        "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+      },
+    },
     server: {
       cors: true,
       proxy: {
